b7872fd53e77dfdfc31e82c8d113f002ef23fe30
aleksandr1512 <143609753+aleksandr1512@users.noreply.github.com>
2024-06-29 13:58:30 +0400

    переписал структуру кода

--- SSK/src/config.asm
+++ SSK/src/config.asm
@@ -1,14 +1,37 @@
-%include "custom.asm"
-
-; 0x0d - символ возварата картки, 0xa - символ новой строки
-greetings: db "The kernel is on.", 0x0d, 0xa, 0xa, 0
-goodbye: db 0x0d, 0xa, "kernel is off", 0x0d, 0xa, 0
-prompt: db "|$|>", 0
+starting_text: db "Kernel is starting...", 0x0d, 0xa, 0
+shutdown_text: db "Kernel is shutdown...", 0x0d, 0xa, 0
+help_text: db "it`s a small simple kernel or SSK", 0
+wrong_text: db "Wrong command!", 0
+prompt: db  ">", 0
 new_line: db 0x0d, 0xa, 0
 
-on_start:	; код запускающийся при старте
-	;ваш код и/или вызов функций из custom.asm
+help_command: db "help", 0
+
+on_start:
+
+	mov si, starting_text   
+    call print 
+
 	ret
-always:		;код работающий в mainloop
-	;ваш код и/или вызов функций из custom.asm
+always:
+
+	mov ax,help_command
+	call check_the_input
+
+	cmp cx,1
+	je equal_help
+
+	jmp equal_to_nothing
+	
 	ret
+
+equal_help:
+    mov si, help_text
+    call print
+
+    jmp done
+
+equal_to_nothing:
+	mov si, wrong_text
+	call print
+    jmp done
--- SSK/src/main.asm
+++ SSK/src/main.asm
@@ -1,48 +1,39 @@
 org 0x7c00
 bits 16
 
-jmp start                     ; сразу переходим в start
+jmp start 
 
-%include "print_string.asm"
-%include "str_compare.asm"
-%include "input_processing.asm"
+%include "func.asm"
 %include "config.asm"
-%include "user_input.asm"
-
-; ====================================================
 
 start:
-    cli                                ; запрещаем прерывания, чтобы наш код 
-                                       ; ничто лишнее не останавливало
+	mov ah, 0x00        
+    mov al, 0x03
+    int 0x10
 
-	mov ah, 0x00
-	mov al, 0x03
-	int 0x10
-    
-    mov sp, 0x7c00            ; инициализация стека
+    mov sp, 0x7c00 
 
-    mov si, greetings         ; печатаем приветственное сообщение
-    call print_string_si      ; после чего сразу переходим в mainloop
 	call on_start
 
-mainloop:
-	call always
+mainloop:	
     mov si, prompt            ; печатаем стрелочку
-    call print_string_si
+    call print
 
+	
     call get_input            ; вызываем функцию ожидания ввода
-    	
-	mov si, new_line          ; печатаем символ новой строки
-    call print_string_si
 
-	call check_the_input
+	
+    mov si, new_line          ; печатаем символ новой строки
+    call print
 
-    jmp mainloop              ; повторяем mainloop...
+	call always
 
+    mov si, new_line          ; печатаем символ новой строки
+    call print
 
+    jmp mainloop
     
-input: times 64 db 0          ; размер буффера - 64 байта
-
+buffer: times 64 db 0          
 
 times 510 - ($-$$) db 0
 dw 0xaa55
